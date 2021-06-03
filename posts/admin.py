d433e8f601d863a23166dba5bfa071c56a192ab9
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-06-03 04:25:30 +0300

    add comments

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from .models import Post, Group
+from .models import Post, Group, Comment
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "author", "post", "created")
+    search_fields = ("text",)
+    list_filter = ("created",)
+    empty_value_display = "-пусто-"
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -17,5 +24,6 @@ class GroupAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
